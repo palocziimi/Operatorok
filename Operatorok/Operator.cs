b28690e9efa5cab001526b3a2d1c0f40daea0b4e
Pálóczi Imre <palocziimi@gmail.com>
2023-05-24 09:55:06 +0200

    Add project files.

---
+++ Operatorok/Operator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operatorok
+{
+    class Operator
+    {
+        private int szam1;
+        private string oper;
+        private int szam2;
+
+        public Operator(int szam1, string oper, int szam2)
+        {
+            this.szam1 = szam1;
+            this.oper = oper;
+            this.szam2 = szam2;
+        }
+
+        public int Szam1 { get => szam1; }
+        public string Oper { get => oper; }
+        public int Szam2 { get => szam2; }
+    }
+}
---
+++ Operatorok/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Operatorok
+{
+    class Program
+    {
+        static List<Operator> lista = new List<Operator>();
+        static void Main(string[] args)
+        {
+            //1.
+            StreamReader sr = new StreamReader("kifejezesek.txt");
+            while (!sr.EndOfStream)
+            {
+                var mezok = sr.ReadLine().Split(' ');
+
+                lista.Add(new Operator(int.Parse(mezok[0]), mezok[1], int.Parse(mezok[2])));
+
+            }
+            sr.Close();
+
+            //2.
+            Console.WriteLine($"2. feladat: Kifejezések száma: {lista.Count()}");
+
+            //3.
+            Console.WriteLine($"3. feladat: Kifejezések maradékos osztással: {lista.Where(x => x.Oper == "mod").Count()}");
+
+            //4.
+            Console.WriteLine("4. feladat: " + (lista.Any(x => x.Szam1 % 10 == 0 && x.Szam2 % 10 == 0) ? "Van ilyen kifejezés!" : "Nincs ilyen kifejezés!"));
+
+            //5.
+            Console.WriteLine("5. feladat: Statisztika");
+
+            lista.GroupBy(x => x.Oper).ToList().ForEach(x => Console.WriteLine($"\t{x.Key} -> {x.Count()}"));
+
+        }
+    }
+}
